a723e2c6b5b3670a4c4f41d6e2ec1d3e97b0541b
papaya <sophorntitheang@gmail.com>
2023-08-15 16:21:22 +0700

    first push

---
+++ Tests/CloneMBCoreTests/CloneMBCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CloneMBCore
+
+final class CloneMBCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CloneMBCore().text, "Hello, World!")
+    }
+}
